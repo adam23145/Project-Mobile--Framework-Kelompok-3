de874e6a0172cfb859ee434f8693a923b561ddae
Adam dwi Julianto <81203366+adam23145@users.noreply.github.com>
2022-06-17 13:40:01 +0700

    update on boarding screen

---
+++ scholineid/lib/models/data.dart
@@ -0,0 +1,54 @@
+
+
+class DataDashboard {
+  String nama;
+  String image;
+
+  DataDashboard({this.nama = '', this.image = ''});
+}
+
+
+var dataDashboard = [
+  DataDashboard(nama: "Chemistry", image: '1.svg'),
+  DataDashboard(nama: "Physics", image: '2.svg'),
+  DataDashboard(nama: "Mathematics", image: '3.svg'),
+  DataDashboard(nama: "Biologi", image: '4.svg'),
+];
+class DataGuru {
+  String nama;
+  String image;
+
+  DataGuru({this.nama = '', this.image = ''});
+}
+
+
+var dataGuru = [
+  DataGuru(nama: "Adam Dj", image: '1.png'),
+  DataGuru(nama: "Alvioni", image: '1.png'),
+  DataGuru(nama: "Alvin", image: '1.png'),
+  DataGuru(nama: "Vivi", image: '1.png'),
+];
+
+
+class DataMateri{
+  String nama;
+  String image;
+
+  DataMateri({this.nama = '', this.image= ''});
+}
+
+var dataMateri = [
+  DataMateri(nama: "Business", image: '1.svg'),
+  DataMateri(nama: "Design", image: '1.svg'),
+  DataMateri(nama: "Health", image: '1.svg'),
+  DataMateri(nama: "Humanity", image: '1.svg'),
+  DataMateri(nama: "Innovation", image: '1.svg'),
+  DataMateri(nama: "lifeStyle", image: '1.svg'),
+  DataMateri(nama: "Technology", image: '1.svg'),
+  DataMateri(nama: "Chemistry", image: '1.svg'),
+  DataMateri(nama: "Physics", image: '2.svg'),
+  DataMateri(nama: "Mathematics", image: '3.svg'),
+  DataMateri(nama: "Biologi", image: '4.svg'),
+   DataMateri(nama: "Business", image: '1.svg'),
+];
+
